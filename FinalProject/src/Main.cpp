c716c312e9f5bd72b0952a327bf5e1bc7518767e
beybladeuser <rubencruz0312@gmail.com>
2021-05-24 16:44:58 +0100

    testing model loading in vs

---
+++ FinalProject/src/Main.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include"Shader.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"EBO.h"
+#include"Model.h"
+
+GLfloat vertices[] =
+{ //               COORDINATES                  /     COLORS           //
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
+};
+
+
+GLuint indices[] =
+{
+	0, 3, 5, 
+	3, 2, 4, 
+	5, 4, 1 
+};
+
+
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Mine-Rush: A Migrant Odyssey", NULL, NULL);
+	
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	
+	glViewport(0, 0, 800, 800);
+
+	Shader shaderProgram("src/Shaders/default.vert", "src/Shaders/default.frag");
+
+	char mPath[] = "Models/rockModel.obj";
+
+	Model m(mPath);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.7f, 0.7f, 0.7f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shaderProgram.Activate();
+
+
+		m.Draw(shaderProgram);
+		
+		glfwSwapBuffers(window);
+		
+		glfwPollEvents();
+	}
+
+	
+	shaderProgram.Delete();
+	
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
---
+++ FinalProject/src/Model.h
@@ -0,0 +1,38 @@
+#ifndef MODEL_CLASS_H
+#define MODEL_CLASS_H
+
+#include <vector>
+#include <string>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <stb/stb_image.h>
+
+#include "Shader.h"
+#include "Mesh.h"
+
+class Model
+{
+public:
+    Model(std::string const& path, bool gamma = false) : gammaCorrection(gamma)
+    {
+        loadModel(path);
+    }
+    void Draw(Shader& shader);
+private:
+    // model data
+    std::vector<Mesh> meshes;
+    std::string directory;
+    std::vector<Texture> textures_loaded;
+    bool gammaCorrection;
+
+    void loadModel(std::string const& path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+        std::string typeName);
+
+    Material loadMaterial(aiMaterial* mat);
+};
+
+#endif
